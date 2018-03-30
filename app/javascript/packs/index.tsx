c6afc46a35b7cb5aadae9a7e72d34b2b2f1a66c5
Sahil Muthoo <sahil.muthoo@gmail.com>
2018-03-30 15:14:21 +0100

    Move stuff out of packs

--- app/javascript/packs/index.tsx
+++ app/javascript/packs/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {Hello} from "./hello_react";
+import {Hello} from "../hello_react";
 
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(
